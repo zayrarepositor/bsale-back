5e035f094efb88a1cefb2786b81d53c7f5083987
Zayra Velasco <zayra.contacto@gmail.com>
2022-10-14 02:52:30 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -14,15 +14,15 @@ const app = express();
 
 app.use(express.json());
 
-// documentation
+// Documentation
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(options)));
 
 app.use('/products', productsRoutes);
 
-// 404 Not Found
+// 404 Not Found Page
 app.use('*', indexRoutes);
 
-//error handler
+// Error handler
 app.use((req, res, next) => {
   next(res.status(404).send('Cannot complete the request'));
 });

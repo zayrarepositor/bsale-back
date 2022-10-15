f1016f14128ad393ea02f1ba7b0a3bd43b476732
Zayra Velasco <zayra.contacto@gmail.com>
2022-10-14 21:15:24 -0300

    response

--- src/app.js
+++ src/app.js
@@ -21,16 +21,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://storebsale.netlify.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+
 // Documentation
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(options)));
 
--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,26 +1,21 @@
 import { selectProducts } from '../services/products.services.js';
 
-import { success, error, serverError } from '../helpers/responses.js';
+import { success, error } from '../helpers/responses.js';
 
 export const getProducts = async (req, res) => {
   const { category, name } = req.query;
   if (category && Number.isNaN(Number(category)))
     return error({ res, message: 'invalid category', status: 400 });
 
-  try {
-    const data = await selectProducts(req, res);
+  const data = await selectProducts(req, res);
 
-    if (data.length === 0) return error({ res, message: 'products not found' });
+  if (data.length === 0) return error({ res, message: 'products not found' });
 
-    return success({
-      res,
-      message: `all products ${
-        (category ? 'category: ' + category : '') +
-        (name ? ' name: ' + name : '')
-      }`,
-      data,
-    });
-  } catch (err) {
-    return serverError({ res, message: err.message });
-  }
+  return success({
+    res,
+    message: `all products ${
+      (category ? 'category: ' + category : '') + (name ? ' name: ' + name : '')
+    }`,
+    data,
+  });
 };
